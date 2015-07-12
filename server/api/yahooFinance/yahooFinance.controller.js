965647ae03d1c079f085e2ac41ee7ae933c425b5
Colin <colinrileytaylor@gmail.com>
2015-07-12 13:38:16 -0400

    renaming yahoo finance module

--- server/api/yahooFinance/yahooFinance.controller.js
+++ server/api/yahooFinance/yahooFinance.controller.js
@@ -1,3 +1,8 @@
 'use strict';
+var yahooFinanceSearch = require('../../logic/yahooFinanceSearch');
+
+exports.search = function  (req, res) {
+  yahooFinanceSearch //TODO: connect this to finance file
+}
 
 
