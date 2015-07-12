df32aba932e70c441e747c29fefc2e40a46ed9c5
Justin <justinowenjacot@icloud.com>
2015-07-12 12:35:38 -0400

    tweaked

--- server/api/yahooFinance/index.js
+++ server/api/yahooFinance/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var controller = require('./yahooFinance.controller');
-
+var yahooFinance = require('yahoo-finance');
 var router = express.Router();
 
 router.post('/', controller.search);
