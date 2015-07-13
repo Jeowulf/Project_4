206332dea1fe8e1c13347dfd2ee065a2ef4d76d3
Colin <colinrileytaylor@gmail.com>
2015-07-13 09:42:01 -0400

    copying API data to DB in bulk but doesnt respond to client

--- server/api/stock/stock.controller.js
+++ server/api/stock/stock.controller.js
@@ -6,10 +6,12 @@ var yahooFinanceSearch = require('../../logic/yahooFinanceSearch');
 
 exports.create = function (req, res) {
   // console.log(req.body.symbol + 'is symbol from postman');
-  var newStock = new Stock();
+
   yahooFinanceSearch(req.body.symbol, function (data, err) { //do API search and wait for data
       if (err) { return handleError(res, err); }
 //TODO: check for Duplicates - validation
+//TODO: refactor this
+      var newStock = new Stock();
       newStock.name = data.name;
       newStock.symbol = data.symbol;
       newStock.lastTradeDate = data.lastTradeDate;
@@ -24,6 +26,39 @@ exports.create = function (req, res) {
        });
     });
 
+}
+
+exports.createSet = function (res) {
+  yahooFinanceMultipleSymbolSearch(function (data, err) { //do API search and wait for data
+      if (err) { return handleError(res, err); }
+//TODO: check for Duplicates - validation
+//TODO: refactor this
+      var stockArray = [];
+      for (var i = 0; i < data.length ; i++) {
+         var newStock = new Stock();
+          newStock.name = data[i].name;
+          newStock.symbol = data[i].symbol;
+          newStock.lastTradeDate = data[i].lastTradeDate;
+          newStock.lastTradePriceOnly = data[i].lastTradePriceOnly;
+          newStock.dividendYield = data[i].dividendYield;
+          newStock.peRatio = data[i].peRatio;
+          console.log(newStock.name + 'is selectedStock');
+
+          // stockArray.push(newStock);
+          newStock.save(function(err, stock) {
+
+          });
+      }
+      res.send('stockArray is saved'); //- trying to send json array after loop is over
+      // console.log(data.length + ' this is data!!');
+      // var newStock = new Stock();
+
+
+      // newStock.save(function (err, stock) {
+      //   if (err) { return handleError(res, err); }
+      //   res.json(201, stock);
+      //  });
+    });
 }
 
 function handleError(res, err) {
