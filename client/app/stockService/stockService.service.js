a373e4350293a220c5b8194f16b83932d9d77164
Colin <colinrileytaylor@gmail.com>
2015-07-13 18:17:52 -0400

    added created stock inventory in client

--- client/app/stockService/stockService.service.js
+++ client/app/stockService/stockService.service.js
@@ -3,7 +3,11 @@
 angular.module('proj4App')
   .service('stockService', function ($http, Auth) {
 
-    this.getInventory = function() {
+
+    this.createStockInventory = function (){
+      return $http.put('/api/stocks');
+    }//creates a stock inventory call this before you cal get inventory!!!
+    this.getInventory = function() { //gets stock inventory from DB
       return $http.get('/api/stocks');
     }
     this.getUser = function() {
