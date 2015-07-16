67dd3fdae8159d7625cf5fee3dbf3aed60f60991
Justin <justinowenjacot@icloud.com>
2015-07-16 13:26:33 -0400

    added tab active functionality, pre-partializing commit

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -5,6 +5,7 @@ angular.module('proj4App')
 
     var that = this;
 
+    that.tab;
     // stockService.getUser();//helper for server
 
     that.createStockInventory = function(){
@@ -53,10 +54,25 @@ angular.module('proj4App')
     portfolioService.getUserPortfolio().success(function(json) {
       that.myPortfolio = json;
       console.log("portfolio to follow: ")
+      // stockNavService.testies();
       console.log(that.myPortfolio.stocksInPortfolio[0].stock.symbol);
+      console.log('Price: ', that.myPortfolio.stocksInPortfolio[0].stock.lastTradePriceOnly);
 
+      var portfolios = that.myPortfolio.stocksInPortfolio;
+      that.total = 0;
+      portfolios.map(function(portfolio) {
+        var price = portfolio.stock.lastTradePriceOnly * portfolio.qty;
+        that.total += price;
+      });
+      console.log(that.total);
     });
   }
+  // that.portFolioTotalValue = function (portfolio) {
+  //   portfolioService.getPortfolioTotal(portfolio);
+  //   console.log("colin says")
+  // }
+  // that.portFolioTotalValue(that.myPortfolio);
+
   //run when controller is instantiated
   that.getStockInventory();
   that.getUserPortfolio();
@@ -101,9 +117,11 @@ angular.module('proj4App')
     that.getTwitterSentiments();
     that.getTwitterWords();
   }
+
   // console.log(that.words + " adf")
   // console.log(that.specificSentiment)
 });
+
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 
---
+++ client/app/dashboard/stockNav.js
@@ -0,0 +1,9 @@
+// 'use strict'
+
+// angular.module('proj4App')
+//   .controller('stockNav', function() {
+//     this.tab = 1;
+//     this.selectTab = function(setTab) {
+//       this.tab = setTab;
+//     };
+//   })
