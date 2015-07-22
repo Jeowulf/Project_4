0c8296681003e7856c9207092ed12c6ba65a81ad
Colin <colinrileytaylor@gmail.com>
2015-07-22 17:54:41 -0400

    printing

--- client/app/dashboard/stockShow/stockShow.controller.js
+++ client/app/dashboard/stockShow/stockShow.controller.js
@@ -38,7 +38,7 @@ angular.module('proj4App')
     portfolioService.getUserPortfolio().success(function(json) {
       that.myPortfolio = json;
       // console.log(JSON.stringify(that.myPortfolio) + "  Is that.myPortfolio");
-      console.log("portfolio to follow: ")
+      console.log("portfolio to follow: " + JSON.stringify(that.myPortfolio.stocksInPortfolio[0]));
       // stockNavService.testies();
       // console.log(that.myPortfolio.stocksInPortfolio[0].stock.symbol);
       // console.log('Price: ', that.myPortfolio.stocksInPortfolio[0].stock.lastTradePriceOnly);
