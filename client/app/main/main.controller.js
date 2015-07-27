1155587fff1c26d78e666cf34e1c37b693f5a5ab
Colin <colinrileytaylor@gmail.com>
2015-07-26 22:31:09 -0400

    cleanup

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,7 +3,46 @@
 angular.module('proj4App')
   .controller('MainCtrl', function () {
     // var that = this;
-    this.links = []
+    this.colinLinks = [{
+      'link': 'mailto:colinrileytaylor@gmail.com',
+      'icon':
+      'class': "yellow darken"
+      },
+      // {
+      // 'title': 'allPortfolios',
+      // 'link': '/allPortfolios',
+      // 'img': ""
+      // },
+     {
+      'link': '/stockinventory',
+      'img': "../../../../assets/images/stockIcon.png",
+      'class': "green"
+      },
+      {
+      'link': '/portfolio',
+      'img': "../../../../assets/images/moneyIcon.png",
+      'class': "blue"
+      }];
 
+       this.justinLinks = [{
+      'link': '/',
+      'img': "../../../../assets/images/homeIcon.png",
+      'class': "yellow darken"
+      },
+      // {
+      // 'title': 'allPortfolios',
+      // 'link': '/allPortfolios',
+      // 'img': ""
+      // },
+     {
+      'link': '/stockinventory',
+      'img': "../../../../assets/images/stockIcon.png",
+      'class': "green"
+      },
+      {
+      'link': '/portfolio',
+      'img': "../../../../assets/images/moneyIcon.png",
+      'class': "blue"
+      }];
 
   });
